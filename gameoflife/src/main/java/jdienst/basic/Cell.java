d4b3b96ba5bfa0286a8e25b776f74d467b2e79cc
Johannes Dienst <johannesdienst@t-online.de>
2017-08-06 19:58:39 +0200

    Work on cell including tests for 100% line coverage

--- gameoflife/src/main/java/jdienst/basic/Cell.java
+++ gameoflife/src/main/java/jdienst/basic/Cell.java
@@ -3,13 +3,40 @@ package jdienst.basic;
 public class Cell {
 
   private boolean isAlive = false;
+  private Cell[] neighbors;
 
-  public Cell() {
-
+  public Cell(int neighborCount) {
+    neighbors = new Cell[neighborCount];
   }
 
   public boolean isAlive() {
     return isAlive;
   }
 
+  public void enlive() {
+    isAlive = true;
+  }
+
+  public Cell setNeighbors(Cell[] neighbors) {
+    this.neighbors = neighbors;
+    return this;
+  }
+
+  public boolean isAliveInNextGeneration() {
+    int livingNeighbors = 0;
+    for (Cell cell : neighbors) {
+      if (cell.isAlive()) {
+        livingNeighbors++;
+      }
+    }
+
+    if (!isAlive && livingNeighbors == 3) {
+      return true;
+    }
+    else if (isAlive && livingNeighbors <= 3) {
+      return true;
+    }
+
+    return false;
+  }
 }
--- gameoflife/src/test/java/jdienst/basic/Cell_Test.java
+++ gameoflife/src/test/java/jdienst/basic/Cell_Test.java
@@ -1,6 +1,7 @@
 package jdienst.basic;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,7 +9,58 @@ public class Cell_Test {
 
   @Test
   public void shouldBeDeadAfterConstruction() {
-    Cell cell = new Cell();
+    Cell cell = new Cell(8);
     assertFalse("Cell is alive. But it should not be alive after construction", cell.isAlive());
+  }
+
+  @Test
+  public void setThreeLivingNeighbors_IsDead_IsAliveInNextGeneration() {
+    Cell cell = getCell(false, 8);
+    Cell[] neighbors = new Cell[8];
+    for (int i = 0; i < 8; i++) {
+      if (i < 3) {
+        neighbors[i] = getCell(true, 8);
+      }
+      else {
+        neighbors[i] = getCell(false, 8);
+      }
+    }
+    cell.setNeighbors(neighbors);
+    assertTrue("Cell should be alive with three living neighbors", cell.isAliveInNextGeneration());
+  }
+
+  @Test
+  public void setThreeLivingNeighbors_IsAlive_IsAliveNextGeneration() {
+    Cell cell = getCell(true, 8);
+    Cell[] neighbors = new Cell[8];
+    for (int i = 0; i < 8; i++) {
+      if (i < 3) {
+        neighbors[i] = getCell(true, 8);
+      }
+      else {
+        neighbors[i] = getCell(false, 8);
+      }
+    }
+    cell.setNeighbors(neighbors);
+    assertTrue("Cell should be alive with three living neighbors", cell.isAliveInNextGeneration());
+  }
+
+  @Test
+  public void setEightLivingNeighbors_IsAlive_IsDeadNextGeneration() {
+    Cell cell = getCell(true, 8);
+    Cell[] neighbors = new Cell[8];
+    for (int i = 0; i < 8; i++) {
+      neighbors[i] = getCell(true, 8);
+    }
+    cell.setNeighbors(neighbors);
+    assertFalse("Cell should be dead with eight living neighbors", cell.isAliveInNextGeneration());
+  }
+
+  public static Cell getCell(boolean alive, int neighborsCount) {
+    Cell cell = new Cell(8);
+    if (alive) {
+      cell.enlive();
+    }
+    return cell;
   }
 }
